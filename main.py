a7c2efe39068173ddd406c5c8f9da35c5bcce6b2
FedorovVladimir <vladimir.fodorow@gmail.com>
2020-10-22 22:03:18 +0700

    clean and add boll_r

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from cv2 import cv2
 
 faceCascade = cv2.CascadeClassifier('Cascades/haarcascade_frontalface_default.xml')
-eyeCascade = cv2.CascadeClassifier('Cascades/haarcascade_eye.xml')
 
 gray_h = 90
 
@@ -13,6 +12,7 @@ boll_center_y = screen_h - 30
 boll_dx = 1
 boll_dy = -1
 boll_speed = 10
+boll_r = 7
 
 
 def find_faces(img):
@@ -21,31 +21,6 @@ def find_faces(img):
         scaleFactor=1.2,
         minNeighbors=5,
         minSize=(20, 20))
-
-
-def find_eyes(img):
-    return eyeCascade.detectMultiScale(
-        img,
-        scaleFactor=1.5,
-        minNeighbors=10,
-        minSize=(5, 5))
-
-
-def find_centers(img):
-    (x, y, w, h) = find_faces(img)[0]
-
-    roi_gray = img[y:y + h, x:x + w]
-    eyes = find_eyes(roi_gray)
-
-    centers = []
-    for (ex, ey, ew, eh) in eyes:
-        eye_x = x + ex
-        eye_y = y + ey
-        center_x = int(eye_x + ew / 2)
-        center_y = int(eye_y + eh / 2)
-        centers.append((center_x, center_y))
-
-    return centers[0], centers[1]
 
 
 def norm(a, min_a, max_a):
@@ -62,17 +37,14 @@ def calc_boll_center(left, right):
     boll_center_x = boll_center_x + boll_speed * boll_dx
     boll_center_y = boll_center_y + boll_speed * boll_dy
 
-    if boll_center_x > screen_w or boll_center_x < 0:
+    if boll_center_x > screen_w - boll_r or boll_center_x < boll_r:
         boll_dx = -boll_dx
 
-    if boll_center_y < 0:
+    if boll_center_y < boll_r:
         boll_dy = -boll_dy
 
-    if boll_center_y > screen_h - 25 and (left < boll_center_x < right):
+    if boll_center_y > screen_h - 20 - boll_r and (left < boll_center_x < right):
         boll_dy = -boll_dy
-
-    if boll_center_y > screen_h:
-        print('govno')
 
 
 def work(img):
@@ -95,6 +67,9 @@ def run():
         img = work(img)
         cv2.imshow("camera", img)
         if cv2.waitKey(10) == 27:  # Клавиша Esc
+            break
+        if boll_center_y > screen_h:
+            print('the end...')
             break
 
 
