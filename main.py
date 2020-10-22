986e0627f8366501b3f12255675239d88fc9ab97
FedorovVladimir <vladimir.fodorow@gmail.com>
2020-10-22 22:35:50 +0700

    add board_f

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import math
+
 from cv2 import cv2
 
 faceCascade = cv2.CascadeClassifier('Cascades/haarcascade_frontalface_default.xml')
@@ -9,7 +11,7 @@ screen_h = 480
 board_w = 100
 boll_center_x = int(screen_w / 2)
 boll_center_y = screen_h - 30
-boll_dx = 1
+boll_dx = 0
 boll_dy = -1
 boll_speed = 12
 boll_r = 8
@@ -35,17 +37,25 @@ def norm(a, min_a, max_a):
 
 def calc_boll_center(left, right):
     global boll_center_x, boll_center_y, boll_dx, boll_dy
-    boll_center_x = boll_center_x + boll_speed * boll_dx
-    boll_center_y = boll_center_y + boll_speed * boll_dy
+    boll_center_x = int(boll_center_x + boll_speed * boll_dx)
+    boll_center_y = int(boll_center_y + boll_speed * boll_dy)
 
-    if boll_center_x > screen_w - boll_r or boll_center_x < boll_r:
+    if boll_center_x > screen_w - boll_r:
         boll_dx = -boll_dx
+        boll_center_x = screen_w - boll_r
+
+    if boll_center_x < boll_r:
+        boll_dx = -boll_dx
+        boll_center_x = boll_r
 
     if boll_center_y < boll_r:
         boll_dy = -boll_dy
+        boll_center_y = boll_r
 
     if boll_center_y > screen_h - 20 - boll_r and (left < boll_center_x < right):
-        boll_dy = -boll_dy
+        d = (boll_center_x - old_center_x) / board_w
+        boll_dy = -math.cos(d)
+        boll_dx = math.sin(d)
 
 
 def work(img):
